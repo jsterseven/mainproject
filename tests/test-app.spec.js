15a33be673c8d6e124c9df1126890afb2c66e0ee
jster42 <slavikabs@gmail.com>
2024-02-15 20:14:22 +0300

    Fixed tests

--- tests/test-app.spec.js
+++ tests/test-app.spec.js
@@ -1,49 +1,52 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-    await page.goto('/catalog/markery/');
+    await page.goto('catalog/knigi-i-kantstovary/kantstovary/bumazhnaya-produktsiya/tetradi/');
+});
+
+test.afterEach(async ({ page }) => {
+    await page.close()
 });
 
 test.describe('UI тесты для корзины', () => {
     test('Добавление товара в корзину', async ({ page }) => {
-        await page.locator('//button[@data-meta-name="Snippet__cart-button"]').first().click();
-        await page.locator('[data-meta-name="BasketButton"]').first().click();
+        await page.locator('.product-card__add-basket').first().click();
+        await page.locator('.j-item-basket').click();
 
-        await expect(page.locator('[data-meta-name="ProductList"]')).toBeVisible();
+        await expect(page.locator('.j-basket-form__content')).toBeVisible();
     });
 
     test('Добавление нескольких позиций товара в корзину', async ({ page }) => {
-        await page.locator('//button[@data-meta-name="Snippet__cart-button"]').first().click();
-        await page.locator('//button[@data-meta-name="Snippet__cart-button"]').nth(2).click();
-        await page.locator('//button[@data-meta-name="Snippet__cart-button"]').nth(3).click();
-        await page.locator('[data-meta-name="BasketButton"]').first().click();
-        await page.waitForLoadState('domcontentloaded');
+        await page.locator('.product-card__add-basket').first().click();
+        await page.locator('.product-card__add-basket').nth(2).click();
+        await page.locator('.product-card__add-basket').nth(3).click();
+        await page.locator('.j-item-basket').click();
+        await page.waitForLoadState('networkidle');
 
-        const countOfProducts = await page.locator('[data-meta-type="Product"]').count();
+        const countOfProducts = await page.locator('.list-item__wrap').count();
         expect(countOfProducts).toEqual(3);
     });
 
     test('Удаление товара из корзины', async ({ page }) => {
-        await page.locator('//button[@data-meta-name="Snippet__cart-button"]').first().click();
-        await page.locator('[data-meta-name="BasketButton"]').first().click();
+        await page.locator('.product-card__add-basket').first().click();
+        await page.locator('.j-item-basket').first().click();
 
-        await page.locator('[data-meta-name="DeleteAction"]').click();
-        await expect(page.getByText('В корзине нет товаров')).toBeVisible();
+        await page.locator('.btn__del').click();
+        await expect(page.getByText('В корзине пока пусто')).toBeVisible();
     });
 
-    test('Очистка корзины через кнопку "Очистить корзину"', async ({ page }) => {
-        await page.locator('//button[@data-meta-name="Snippet__cart-button"]').nth(2).click();
-        await page.locator('//button[@data-meta-name="Snippet__cart-button"]').nth(3).click();
-        await page.locator('[data-meta-name="BasketButton"]').first().click();
+    test('Отображение счётчика количества товаров', async ({ page }) => {
+        await page.locator('.product-card__add-basket').nth(2).click();
+        await page.locator('.product-card__add-basket').nth(3).click();
+        await page.locator('.j-item-basket').click();
 
-        await page.getByText('Очистить корзину').click();
-        await expect(page.getByText('В корзине нет товаров')).toBeVisible();
+        await expect(page.locator('h1.basket-section__header')).toHaveAttribute('data-count', '2');
     });
 
-    test('Переход к каталогу из корзины через кнопку "Вернуться к покупкам"', async ({ page }) => {
-        await page.locator('[data-meta-name="BasketButton"]').first().click();
+    test('Переход к каталогу из корзины через кнопку "Перейти на главную"', async ({ page }) => {
+        await page.locator('.j-item-basket').click();
 
-        await page.locator('//a[text()="Вернуться к покупкам"]').click();
-        await expect(page).toHaveURL(/https:\/\/www.citilink.ru\/$/);
+        await page.locator('//a[text()="Перейти на главную"]').click();
+        await expect(page).toHaveURL(/https:\/\/www.wildberries.ru\/$/);
     });
 });
